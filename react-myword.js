14c07024b00cc6b4c791e9c59f169e7875d821a8
Kirkwood, James <jkirkwood@paychex.com>
2018-12-27 14:14:14 -0500

    filling correct cells when typing

--- react-myword.js
+++ react-myword.js
@@ -1,3 +1,21 @@
+const fetchWordLists = () => {
+    var self = this;
+    var p = new Promise(function(resolve, reject) {
+        var x1 = new XMLHttpRequest();
+        x1.addEventListener('load', function () {
+            sixWords = eval(x1.responseText);
+            resolve();
+        });
+        x1.open('GET', 'sixes.json');
+        x1.overrideMimeType('application/json');
+        x1.send();
+    });
+
+    return p;
+}
+
+const pickWord = (words) => words[Math.floor(Math.random() * (words.length))].toLowerCase();
+
 const UnusedCell = (props) => {
     return (
         <div className="cell unused"></div>
@@ -6,13 +24,13 @@ const UnusedCell = (props) => {
 
 const GuessCell = (props) => {
     return (
-        <div className="cell letter"></div>
+        <div className="cell letter">{props.value}</div>
     );
 };
 
 const ScoreCell = (props) => {
     return (
-        <div className="cell score"></div>
+        <div className="cell score">{props.score}</div>
     );
 };
 
@@ -20,45 +38,91 @@ const GuessRow = (props) => {
     return (
         <div className="row">
             { props.spec.map( (cell, index) => {
-                if (cell === 'u') return <UnusedCell key={index} />;
-                else if (cell === 'g') return <GuessCell key={index} />; })
+                if (cell.type === 'u') return <UnusedCell key={index} />;
+                else if (cell.type === 'g') return <GuessCell key={index} value={cell.value}/>; })
             }
-            <ScoreCell />
+            <ScoreCell score={props.score} />
         </div>
     );
 }
 
+
+
 const GameTable = (props) => {
     return (
         <div className="table">
-        {console.log(props.structure) }
-            { props.structure.map( (row, index) => <GuessRow key={index} spec={row} /> ) }
+        {console.log(props.model) }
+            { props.model.map( (row, index) => <GuessRow key={index} spec={row} score={props.score[index]} /> ) }
         </div>
     );
 }
 
 class MyWordApp extends React.Component {
-    tableStructure = [['g','g','u','u','u','u'],
-                      ['g','g','g','u','u','u'],
-                      ['u','g','g','g','u','u'],
-                      ['u','u','g','g','g','u'],
-                      ['u','u','u','g','g','g'],
-                      ['u','u','g','g','g','g'],
-                      ['u','g','g','g','g','u'],
-                      ['g','g','g','g','u','u'],
-                      ['g','g','g','g','g','u'],
-                      ['u','g','g','g','g','g'],
-                      ['g','g','g','g','g','g']];
 
     state = { activeRow: 0,
               activeCell: 0,
-              targetWord: 'grapes' };
+              completed: false,
+              targetWord: 'grapes', // TODO: initialize with random choice
+              score: [null,null, null, null, null, null, null, null, null, null, null],
+             tableModel: [[{type: 'g', value: null, start:true},{type: 'g', value: null, last:true},{type:'u'},{type:'u'},{type:'u'},{type:'u'}],
+                          [{type: 'g', value: null, start:true},{type: 'g', value: null},{type: 'g', value: null, last:true},{type:'u'},{type:'u'},{type:'u'}],
+                          [{type:'u'},{type: 'g', value: null, start:true},{type: 'g', value: null},{type: 'g', value: null, last:true},{type:'u'},{type:'u'}],
+                          [{type:'u'},{type:'u'},{type: 'g', value: null, start:true},{type: 'g', value: null},{type: 'g', value: null, last:true},{type:'u'}],
+                          [{type:'u'},{type:'u'},{type:'u'},{type: 'g', value: null, start:true},{type: 'g', value: null},{type: 'g', value: null, last:true}],
+                          [{type:'u'},{type:'u'},{type: 'g', value: null, start:true},{type: 'g', value: null},{type: 'g', value: null},{type: 'g', value: null, last:true}],
+                          [{type:'u'},{type: 'g', value: null, start:true},{type: 'g', value: null},{type: 'g', value: null},{type: 'g', value: null, last:true},{type:'u'}],
+                          [{type: 'g', value: null, start:true},{type: 'g', value: null},{type: 'g', value: null},{type: 'g', value: null, last:true},{type:'u'},{type:'u'}],
+                          [{type: 'g', value: null, start:true},{type: 'g', value: null},{type: 'g', value: null},{type: 'g', value: null},{type: 'g', value: null, last:true},{type:'u'}],
+                          [{type:'u'},{type: 'g', value: null, start:true},{type: 'g', value: null},{type: 'g', value: null},{type: 'g', value: null},{type: 'g', value: null, last:true}],
+                          [{type: 'g', value: null, start:true},{type: 'g', value: null},{type: 'g', value: null},{type: 'g', value: null},{type: 'g', value: null},{type: 'g', value: null, last:true, complete:true}]]
+    };
+
+    computeNextLocation = (currentState) => {
+        var nextCellIx, nextRowIx,
+            currentCell = currentState.tableModel[currentState.activeRow][currentState.activeCell],
+            completed = false;
+        if (currentCell.complete) {
+            completed = true;
+            nextCellIx = currentState.activeCell;
+            nextRowIx = currentState.activeRow;
+        } else if (currentCell.last) {
+            // move to next line and score
+            nextRowIx = currentState.activeRow + 1;
+            nextCellIx = _.findIndex(currentState.tableModel[nextRowIx], cell => cell.start);
+        } else {
+            nextCellIx = currentState.activeCell + 1;
+            nextRowIx = currentState.activeRow;
+        }
+
+        return { activeRow: nextRowIx,
+                 activeCell: nextCellIx,
+                 "completed": completed };
+    }
+
+    handleEntry = (event) => {
+        event.preventDefault();
+        console.log('entered', event.key);
+        // TODO: check for valid entries
+        this.setState((currentState) => {
+            // TODO: score when the row is full
+            var newtable = _.cloneDeep(currentState.tableModel);
+            newtable[currentState.activeRow][currentState.activeCell].value = event.key;
+            return _.merge({tableModel: newtable}, this.computeNextLocation(currentState));
+        });
+    };
 
 
     render() {
         return (
-            <GameTable structure={this.tableStructure} />
+            <GameTable model={this.state.tableModel} score={['','','','','','','','','','','']} />
         );
+    }
+
+    componentDidMount() {
+        window.addEventListener('keydown', this.handleEntry);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.handleEntry);
     }
 }
 
