b01da297e6a84994a3f3342eb6e9408fab0791ae
ceenlrnjim <you@example.com>
2022-02-28 13:23:50 -0500

    fix useEffect dependencies

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -24,7 +24,7 @@ function Keyboard(props) {
                 props.onLetterGuess(event.key.toUpperCase());
             }
         }
-    });
+    },[]);
 
     return  (
         <div className="keyboard">
