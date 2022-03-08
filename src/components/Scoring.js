a4274e56fdd1a7c6861653cbe4d825c753f9aee7
ceenlrnjim <you@example.com>
2022-03-07 20:20:28 -0500

    removed alternate scoring approach

--- src/components/Scoring.js
+++ src/components/Scoring.js
@@ -28,30 +28,7 @@ function score(target, guess, start, len) {
     for (i=0;i<targetMatch.length;i++) {
         score += targetMatch.charAt(i) === '~' ? 25 : 0;
     }
-
-    console.log("score comparison: ", score, score2(target, guess, start, len));
-
     return score;
-}
-
-function dropFirst(arr, val) {
-    const index = arr.findIndex(v => v===val);
-    if (index >= 0) {
-        arr.splice(index,1)
-    }
-    return arr;
-}
-
-function score2(target, guess, start, len) {
-    // TODO: need the other letters that aren't in the guess range as they can match for inexact points
-    const unmatchedLetters = guess.split('').filter((l,ix) => l !== target[ix + start]);
-    const exactMatchScore = (len - unmatchedLetters.length)*100;
-    const {score} = unmatchedLetters.reduce(({score, remaining},v) => {
-        return {score: score + (remaining.includes(v) ? 25: 0),
-                remaining: dropFirst(remaining, v)};
-    }, {score:0, remaining: guess.split('')})
-
-    return exactMatchScore + score;
 }
 
 export default score;
